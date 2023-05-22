26a5b23220dbb46e144a9411739ecf675be9a2e5
Ray <rei.piyo.55687@gmail.com>
2023-05-23 03:50:49 +0900

    公文式課題5

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float _speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ç∂âEà⁄ìÆ
+        float hInput = Input.GetAxisRaw("Horizontal");
+        transform.Translate(Vector2.right * hInput * _speed);
+        //ÉWÉÉÉìÉv
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+    }
+
+    void Jump()
+    {
+        for(float theta = 0f; theta <= Mathf.PI; theta += (Mathf.PI / 180))
+        {
+            transform.Translate(Vector2.up * Mathf.Sin(theta));
+        }
+    }
+}

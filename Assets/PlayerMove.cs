b3d7b3450b1173e6204b60fe6661c83e7a3c2806
Ray <rei.piyo.55687@gmail.com>
2023-05-23 21:10:23 +0900

    修正

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] float _speed = 0.1f;
+    [SerializeField] float _jumpTime = 0f;
+    [SerializeField] float _jumpHeight = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,18 +20,18 @@ public class PlayerMove : MonoBehaviour
         float hInput = Input.GetAxisRaw("Horizontal");
         transform.Translate(Vector2.right * hInput * _speed);
         //ƒWƒƒƒ“ƒv
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Jump();
+            StartCoroutine(Jump());
         }
 
     }
-
-    void Jump()
+    IEnumerator Jump()
     {
-        for(float theta = 0f; theta <= Mathf.PI; theta += (Mathf.PI / 180))
-        {
-            transform.Translate(Vector2.up * Mathf.Sin(theta));
-        }
+        Vector2 dir = new Vector2(transform.position.x, transform.position.y + _jumpHeight);
+        Vector2 initialPos = transform.position;
+        transform.position = Vector2.Lerp(transform.position, dir, _jumpTime / 2);
+        yield return new WaitForSeconds(_jumpTime / 2);
+        transform.position = Vector2.Lerp(transform.position, initialPos, _jumpTime / 2);
     }
 }
